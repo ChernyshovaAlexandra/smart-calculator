f4189afade6d5da4e9ddc38cfc599011719fa3f3
Alexandra <alexandrachrystal@gmail.com>
2018-03-07 14:23:09 +0300

    function next testing

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.value = initialValue;
-    this.String = initialValue.toString();
-    this.regExp = /(\d+)(?!.*\d)/;
+    this.value = initialValue.toString();
+    
   }
 
   add(number) {
-    this.calc(number, '+')
+    this.calc(number, '+');
     return this;
   }
 
   subtract(number) {
-    this.calc(number, '-')
+    this.calc(number, '-');
     return this;
   }
 
   multiply(number) {
-    this.calc(number, '*')
+    this.calc(number, '*');
     return this;
   }
 
   devide(number) {
-    this.calc(number, '/')
+    this.calc(number, '/');
     return this;
   }
 
   pow(number) {
-    this.calc(number, '**')
+    this.calc(number, '**');
     return this;
   }
-  
+
   calc(number, operator) {
-    this.String = this.String + operator + number
-    this.value = eval(this.String)
+    this.value = this.value + operator + number;
+    this.value = eval(this.value);
+    return this.value
   }
 }
 
